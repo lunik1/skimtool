3d1052a1d83114270e85be713cafcdd13c376d2a
Alexander <alexander.david.morton@cern.ch>
2015-08-18 18:45:46 +0100

    Updated makeDatasetSkim.cpp

--- makeDatasetSkim.cpp
+++ makeDatasetSkim.cpp
@@ -8,61 +8,75 @@
 int main(int argc, char* argv[]){
   
   std::string fileName = argv[1];
-  std::string outFileName = argv[2];
+  std::string datasetName = argv[2];
 
   std::ifstream fileList(fileName.c_str());
   std::string line;             
 
 
-  TChain * datasetChain = new TChain("tree");
-
-  //Fill chain with provided files
+  int fileNum = 0;
   while(getline(fileList,line)){
-    datasetChain->Add(line.c_str());
-  }
-   
-  //Make the output skim tree
-  TTree * outTree = datasetChain->CloneTree(0);
 
-  //Make the output file
-  TFile outFile(outFileName.c_str(),"RECREATE");
-  
-  int numberOfEvents = datasetChain->GetEntries();
-  AnalysisEvent * event = new AnalysisEvent(false,"",datasetChain);
-  for (int i = 0; i < numberOfEvents; i++){
-    //Simple status bar to see how far through we are
-    if (i % 500 < 0.01) std::cerr << i << "/" << numberOfEvents << " (" << 100*float(i)/numberOfEvents << "%)\r";
+    std::string numName, numNamePlus;
+    std::ostringstream convert, convert1;
+    convert << fileNum;
+    convert1 << fileNum+2;
+    numName = convert.str();
+    numNamePlus = convert1.str();
     
-    event->GetEntry(i);
-    int numLeps = 0;
-    //Make some electron cuts
-    for (int j = 0; j < event->numElePF2PAT; j++){
-      if (event->elePF2PATPT[j] < 9) continue;
-      if (fabs(event->elePF2PATEta[j]) > 2.7) continue;
-      if (event->elePF2PATComRelIsoRho[j]/event->elePF2PATPT[j] > 0.5) continue;
-      if (event->elePF2PATMVA[j] < 0.) continue;
-      numLeps++;
+    struct stat buffer;
+    if (stat(("/nfs/data/tZqSkims/" + datasetName + "/skimFile"+numNamePlus+".root").c_str(), &buffer) == 0) {
+      fileNum++;
+      continue;
     }
-    //Make some muon cuts
-    for (int j = 0; j < event->numMuonPF2PAT; j++){
-      if (event->muonPF2PATPt[j] < 9) continue;
-      if (event->muonPF2PATComRelIsodBeta[j] > 0.5) continue;
-      if (fabs(event->muonPF2PATEta[j]) > 2.8) continue;
-      numLeps++;
-    }
-    //Fill output tree
-    if (numLeps >= 2) outTree->Fill();
-  }
-  
-  //write output tree
-  outFile.cd();
-  outTree->Write();
-  outFile.Write();
-  outFile.Close();
-  //    inFile.Close();
 
-  delete datasetChain;
-  delete event;
-  delete outTree;
+    TChain * datasetChain = new TChain("makeTopologyNtuple/tree");
+    datasetChain->Add(line.c_str());
+    
+    //    std::cout << line;
+    //TFile inFile(line.c_str());
+    
+
+    TTree * outTree = datasetChain->CloneTree(0);
+
+    //stupid long conversion between int and string.
+
+    TFile outFile(("/nfs/data/tZqSkims/" + datasetName + "/skimFile"+numName+".root").c_str(),"RECREATE");
+  
+    int numberOfEvents = datasetChain->GetEntries();
+    AnalysisEvent * event = new AnalysisEvent(false,"",datasetChain);
+    for (int i = 0; i < numberOfEvents; i++){
+      if (i % 500 < 0.01) std::cerr << i << "/" << numberOfEvents << " (" << 100*float(i)/numberOfEvents << "%)\r";
+    
+      event->GetEntry(i);
+      int numLeps = 0;
+      for (int j = 0; j < event->numElePF2PAT; j++){
+	if (event->elePF2PATPT[j] < 9) continue;
+	if (fabs(event->elePF2PATEta[j]) > 2.7) continue;
+	if (event->elePF2PATComRelIsoRho[j]/event->elePF2PATPT[j] > 0.5) continue;
+	if (event->elePF2PATMVA[j] < 0.) continue;
+	numLeps++;
+      }
+      for (int j = 0; j < event->numMuonPF2PAT; j++){
+	if (event->muonPF2PATPt[j] < 9) continue;
+	if (event->muonPF2PATComRelIsodBeta[j] > 0.5) continue;
+	if (fabs(event->muonPF2PATEta[j]) > 2.8) continue;
+	numLeps++;
+      }
+      if (numLeps >= 2) outTree->Fill();
+    }
+  
+    outFile.cd();
+    outTree->Write();
+    outFile.Write();
+    outFile.Close();
+    //    inFile.Close();
+
+    delete datasetChain;
+    delete event;
+    delete outTree;
+    fileNum++;
+  }
+
 }
 
