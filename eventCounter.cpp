bb8c5bc0f29b2d8f3daea2dbee274124ca162a57
Alexander <alexander.david.morton@cern.ch>
2016-04-27 10:16:27 +0100

    Fixed memory leak issues.

--- eventCounter.cpp
+++ eventCounter.cpp
@@ -15,27 +15,18 @@ int main(int argc, char* argv[]) {
   std::string line;             
   uint totalEvents(0);
 
-  int fileNum = 0;
   while(getline(fileList,line)){
 
-    std::string numName, numNamePlus;
-    std::ostringstream convert, convert1;
-    convert << fileNum;
-    convert1 << fileNum+2;
-    numName = convert.str();
-    numNamePlus = convert1.str();
-    
     TChain * datasetChain = new TChain("makeTopologyNtupleMiniAOD/tree");
     datasetChain->Add(line.c_str());
     
-    //    std::cout << line;
-    //TFile inFile(line.c_str());
     uint numberOfEvents = datasetChain->GetEntries();
     totalEvents += numberOfEvents;
+
+    delete datasetChain;
   }
 
   std::cout << "Found " << totalEvents << " events." << std::endl;
 
   std::cout << "\nFinished." << std::endl;
-
 }
