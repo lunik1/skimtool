f71d8cb5cb80e006b5bd51674dfebee37f1ab592
Alexander <alexander.david.morton@cern.ch>
2016-04-27 14:17:28 +0100

    Added rel iso cut back in.

--- makeDatasetSkim.cpp
+++ makeDatasetSkim.cpp
@@ -53,12 +53,12 @@ int main(int argc, char* argv[]){
       for (int j = 0; j < event->numElePF2PAT; j++){
 	if (event->elePF2PATPT[j] < 9) continue;
 	if (fabs(event->elePF2PATEta[j]) > 2.7) continue;
-//	if (event->elePF2PATComRelIsoRho[j]/event->elePF2PATPT[j] > 0.5) continue;
+	if (event->elePF2PATComRelIsoRho[j]/event->elePF2PATPT[j] > 0.5) continue;
 	numLeps++;
       }
       for (int j = 0; j < event->numMuonPF2PAT; j++){
 	if (event->muonPF2PATPt[j] < 9) continue;
-//	if (event->muonPF2PATComRelIsodBeta[j] > 0.5) continue;
+	if (event->muonPF2PATComRelIsodBeta[j] > 0.5) continue;
 	if (fabs(event->muonPF2PATEta[j]) > 2.8) continue;
 	numLeps++;
       }
