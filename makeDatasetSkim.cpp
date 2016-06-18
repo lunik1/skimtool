18df39f5e207b7a926354ade928fb8f995dec826
Alexander <alexander.david.morton@cern.ch>
2016-06-18 15:40:17 +0100

    Updated to save amcatnlo weighting info.

--- makeDatasetSkim.cpp
+++ makeDatasetSkim.cpp
@@ -4,15 +4,20 @@
 #include"AnalysisEvent.h"
 #include<sstream>
 #include<sys/stat.h>
+#include<TH1.h>
 
 int main(int argc, char* argv[]){
   
   std::string fileName = argv[1];
   std::string datasetName = argv[2];
+  bool isMC (false);
+  if ( argc == 4 ) isMC = argv[3];
 
   std::ifstream fileList(fileName.c_str());
   std::string line;             
 
+  unsigned int summedWeights [14] {};
+  TH1I* weightHisto = new TH1I ("sumNumPosMinusNegWeights","sumNumPosMinusNegWeights", 7, -3.5, 3.5);
 
   int fileNum = 0;
   while(getline(fileList,line)){
@@ -34,8 +39,7 @@ int main(int argc, char* argv[]){
     datasetChain->Add(line.c_str());
     
     //    std::cout << line;
-    //TFile inFile(line.c_str());
-    
+    //TFile inFile(line.c_str());    
 
     TTree * outTree = datasetChain->CloneTree(0);
 
@@ -44,11 +48,30 @@ int main(int argc, char* argv[]){
     TFile outFile(("/scratch/data/tZqSkimsRun2016/" + datasetName + "/skimFile"+numName+".root").c_str(),"RECREATE");
   
     int numberOfEvents = datasetChain->GetEntries();
-    AnalysisEvent * event = new AnalysisEvent(false,"",datasetChain);
+    AnalysisEvent * event = new AnalysisEvent(isMC,"",datasetChain);
     for (int i = 0; i < numberOfEvents; i++){
       if (i % 500 < 0.01) std::cerr << i << "/" << numberOfEvents << " (" << 100*float(i)/numberOfEvents << "%)\r";
     
       event->GetEntry(i);
+
+      // Get number of positive and negative amc@nlo weights
+      if ( isMC ) {
+	if ( event->origWeightForNorm >= 0.0 ) summedWeights[0]++;
+	else summedWeights[1]++;
+	if ( event->weight_muF0p5 >= 0.0 ) summedWeights[2]++;
+	else summedWeights[3]++;
+	if ( event->weight_muR0p5 >= 0.0 ) summedWeights[4]++;
+	else summedWeights[5]++;
+	if ( event->weight_muF0p5muR0p5 >= 0.0 ) summedWeights[6]++;
+	else summedWeights[7]++;
+	if ( event->weight_muF2 >= 0.0 ) summedWeights[8]++;
+	else summedWeights[9]++;
+	if ( event->weight_muR2 >= 0.0 ) summedWeights[10]++;
+	else summedWeights[11]++;
+	if ( event->weight_muF2muR2 >= 0.0 ) summedWeights[12]++;
+	else summedWeights[13]++;
+      }
+
       int numLeps = 0;
       for (int j = 0; j < event->numElePF2PAT; j++){
 	if (event->elePF2PATPT[j] < 9) continue;
@@ -63,14 +86,24 @@ int main(int argc, char* argv[]){
 	numLeps++;
       }
       if (numLeps >= 2) outTree->Fill();
-    }
-  
+    } 
+
+    weightHisto->Fill(0., summedWeights[0]-summedWeights[1]);
+    weightHisto->Fill(-1., summedWeights[2]-summedWeights[3]);
+    weightHisto->Fill(-2., summedWeights[4]-summedWeights[5]);
+    weightHisto->Fill(-3., summedWeights[6]-summedWeights[7]);
+    weightHisto->Fill(1., summedWeights[8]-summedWeights[9]);
+    weightHisto->Fill(2., summedWeights[10]-summedWeights[11]);
+    weightHisto->Fill(3., summedWeights[12]-summedWeights[13]);
+
     outFile.cd();
     outTree->Write();
+    if (isMC) weightHisto->Write();
+
     outFile.Write();
     outFile.Close();
     //    inFile.Close();
-
+    
     delete datasetChain;
     delete event;
     delete outTree;
